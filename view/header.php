580dd6a46c6ebb1ca0cf12c21daed20ffa32767a
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2022-08-03 21:01:21 +0430

    first commit create sign in & sign up page

---
+++ view/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" href="img/rsz_logo.jpg" type="image/x-icon">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Media Telegraph</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="view/css/js-persian-cal.css">
+    <link rel="stylesheet" href="view/css/scss_style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="view/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body class="bg-dark">
---
+++ view/js/scripts.js
@@ -0,0 +1,18 @@
+var objCal1 = new AMIB.persianCalendar( 'pcal1',
+            { extraInputID: "extra", extraInputFormat: "YYYYMMDD" }
+        );
+
+let calendar_btn = document.getElementsByClassName("pcalBtn");
+console.log(calendar_btn);
+calendar_btn[0].setAttribute("id", "para-1");
+var elem = document.createElement("img");
+elem.setAttribute("src", "view/img/icon.png");
+elem.setAttribute("height", "38");
+elem.setAttribute("width", "38");
+elem.setAttribute("alt", "calendar-icon");
+calendar_btn[0].appendChild(elem);
+let calendar = document.getElementById("calendar");
+calendar.appendChild(calendar_btn[0]);
+console.log(calendar);
+
+
