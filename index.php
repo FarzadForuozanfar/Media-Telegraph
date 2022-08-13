f64e1ee633520156cd49cfcd8adfcc0c287a6fea
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2022-08-13 15:13:28 +0430

    update router

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
 <?php
-
+    session_start();
+    
     $request = $_SERVER["REQUEST_URI"];
     switch ($request) {
         case("/social_network"):
         case("/social_network/"):
-        case("/social_network/index.php"):
-        case("/social_network/index.php/"):
-        case("/social_network/signup.php"):
-        case("/social_network/login.php"):
+        case("/social_network/index"):
+        case("/social_network/index/"):
             require __DIR__."/view/index.php";
-            //echo __DIR__."/view/index.php";
             break;
+
+        case("/social_network/register"):
+            require __DIR__."/controller/register.php";
+            break;  
+
+        case("/social_network/signup"):
+            require __DIR__."/view/signup.php";
+            break; 
+        
+        case("/social_network/login"):
+        case("/social_network/signin"):
+            require __DIR__."/controller/login.php";
+            break;
+        
+        case("/social_network/home"):
+            require __DIR__."/view/home.php";
+            break;
+
+        case("/social_network/newPost"):
+            require __DIR__."/controller/new_post.php";
+            break;
+        
         default:
             require __DIR__."/view/404.php";
             break;
