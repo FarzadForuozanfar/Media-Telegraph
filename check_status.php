72d731143725d9688e7bb14b06ca618d07f651da
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2022-08-26 14:16:42 +0430

    Delete check_status.php

--- check_status.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if(isset($_SESSION['username_login']))
-    {
-        if($_SESSION['username_login']['login_statuse'] != true)
-            header("Location:index");
-    }
-    else
-        header("Location:index");
-?>
