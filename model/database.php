c14f84f168c95200a46b14cbf7b5437771837c3c
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2022-08-30 21:06:02 +0430

    Update

--- model/database.php
+++ model/database.php
@@ -1,6 +1,6 @@
 <?php
-    //$db = new mysqli("localhost","id19275153_farzad","|_2-]?58x/Q!e(IR","id19275153_keep_notes");
-    $db = new mysqli("localhost","root","","social_media");
+    $db = new mysqli("localhost","root"," ","social_media");
+    
     if($db->connect_error)
     {
         echo $db->connect_error;
